e7b60cadd1db46cccca04d69b07d165f89d3a479
C.Lee Taylor <leet@tari.com>
2024-06-11 17:35:09 +0200

    ci(fix): target only debug/osx

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+  let _ = fix_path_env::fix(); // fixes the app from crashing on macOS, see https://github.com/tauri-apps/tauri/issues/7063
+
   // https://docs.rs/diesel_migrations/latest/diesel_migrations/macro.embed_migrations.html#automatic-rebuilds
   println!("cargo:rerun-if-changed=./migrations");
   wallet_daemon_test_lib::run()
